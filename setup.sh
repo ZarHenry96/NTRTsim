254c47ee6953de737011c9dacc644bfe47aac6f3
danibix95 <dani95bissoli@gmail.com>
2019-11-20 16:22:34 +0100

    Updated setup script.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,25 @@
 #   INSTALLATION SCRIPT   #
 # ======================= #
 
+gen_conf='INSTALL_PREFIX="/usr/local"'
+
+if [ $# -gt 0 ]; then
+    if [ "$1" == "local" ]; then
+      echo -e "\nInstallation files will be placed in a local folder\n"
+      # install NTRT library in a custom location
+      ADD_FLAGS='-DCMAKE_INSTALL_PREFIX="../env" -DCMAKE_INSTALL_NAME_DIR="../env"'
+      # updates general dependencies configuration
+      gen_conf='INSTALL_PREFIX="$ENV_DIR"'
+    else
+      echo -e "\nInstallation files will be placed in the system folder\n"
+    fi
+fi
+
+# updates general dependencies configuration
+if test ! -f "external/conf/general.conf"; then
+    echo ${gen_conf} > external/conf/general.conf
+fi
+
 # build external dependencies
 echo "Installing library dependencies"
 pushd external > /dev/null
@@ -12,16 +31,18 @@ pushd external > /dev/null
 ./setup_dependencies.sh
 
 # build and install library dependencies
-./setup_dependencies.sh
+./setup_dependencies.sh || { echo -e "\nError installing the dependencies -- exiting now."; exit 1; }
+
 popd
+# ======================= #
 
 # build NTRT library
 echo "Configuring library build..."
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release || { echo "Error while configuring the build -- exiting now."; exit 1; }
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release ${ADD_FLAGS} || { echo -e "\nError while configuring the build -- exiting now."; exit 1; }
 echo "Done!"
 echo "Building the library..."
-cmake --build build -j || { echo "Error while building the library -- exiting now."; exit 1; }
+cmake --build build -j || { echo -e "\nError while building the library -- exiting now."; exit 1; }
 echo "Done!"
 echo "Installing the library..."
-cmake --install build || { echo "Error while installing the library -- exiting now."; exit 1; }
+cmake --install build || { echo -e "\nError while installing the library -- exiting now."; exit 1; }
 echo "Done!"
